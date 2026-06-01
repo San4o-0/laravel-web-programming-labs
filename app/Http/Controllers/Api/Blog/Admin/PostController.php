fd1597ea4ea24ac4a5ace0c19f5c2b7bae466e7c
San404🫥 <oleksandr.tkach@oa.edu.ua>
2026-06-01 10:54:44 +0300

    feat: complete lab 9 admin blog post management

--- app/Http/Controllers/Api/Blog/Admin/PostController.php
+++ app/Http/Controllers/Api/Blog/Admin/PostController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers\Api\Blog\Admin;
 
+use App\Http\Requests\BlogPostUpdateRequest;
+use App\Repositories\BlogCategoryRepository;
 use App\Repositories\BlogPostRepository;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends BaseController
 {
-    public function __construct(private BlogPostRepository $blogPostRepository)
-    {
+    public function __construct(
+        private BlogPostRepository $blogPostRepository,
+        private BlogCategoryRepository $blogCategoryRepository
+    ) {
         // parent::__construct();
     }
 
@@ -33,9 +39,34 @@ class PostController extends BaseController
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(BlogPostUpdateRequest $request, string $id)
     {
-        //
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) {
+            return ['message' => "Запис id=[{$id}] не знайдено"];
+        }
+
+        $data = $request->validated();
+
+        if (empty($data['slug'])) {
+            $data['slug'] = Str::slug($data['title']);
+        }
+
+        if (empty($item->published_at) && !empty($data['is_published'])) {
+            $data['published_at'] = Carbon::now();
+        }
+
+        $result = $item->update($data);
+
+        if ($result) {
+            return [
+                'success' => true,
+                'message' => 'Успішно збережено',
+                'data' => $item->fresh(['category:id,title', 'user:id,name']),
+            ];
+        }
+
+        return ['message' => 'Помилка збереження'];
     }
 
     /**
